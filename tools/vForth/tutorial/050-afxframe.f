54f503ad2c8e0dfae07003e94d2fed59093a695a
Matteo <matteo_vitturi@yahoo.com>
2026-07-02 10:28:28 +0200

    tut afx

--- tools/vForth/tutorial/050-afxframe.f
+++ tools/vForth/tutorial/050-afxframe.f
@@ -27,6 +27,7 @@ CR
 .(     Type NEWTASK to unload.            ) CR
 
 NEEDS INTERRUPTS
+NEEDS [']
 
 \ ===========================================================================
 \ 1. The .afx format
@@ -165,14 +166,14 @@ NEEDS AFXFRAME
     AFX-CH-DESC !         \ set data pointer for channel 1
     AYSETUP               \ silence and enable all chips
     ISR-OFF
-    ' AFXFRAME ISR-XT !   \ install AFXFRAME as ISR
+    ['] AFXFRAME ISR-XT ! \ install AFXFRAME as ISR
     ISR-ON
     ." AFX player running. BREAK to stop." CR
 ;
 
 : AFX-STOP  ( -- )
     ISR-OFF
-    ' NOOP ISR-XT !
+    ['] NOOP ISR-XT !
     AYSETUP               \ silence all chips
 ;
 
