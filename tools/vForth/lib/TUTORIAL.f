c4c2af35a47763ee7fb1046f9bab90a7e1ec931d
Matteo Vitturi <44555162+mattsteeldue@users.noreply.github.com>
2026-07-18 23:59:01 +0200

    helloworld

--- tools/vForth/lib/TUTORIAL.f
+++ tools/vForth/lib/TUTORIAL.f
@@ -111,9 +111,10 @@ CREATE TUT-TABLE
     H" tutorial/054-blocks-as-assets.f" ,
     H" tutorial/055-afx-sound-board.f" ,
     H" tutorial/056-layer2-palette.f"  ,
-\   H" tutorial/057-dma.f"             ,
+    H" tutorial/057-dot-commands.f"    ,
+\   H" tutorial/058-dma.f"             ,
 
-56 CONSTANT TUT-MAX
+57 CONSTANT TUT-MAX
 
 
 \ ---------------------------------------------------------------------------
